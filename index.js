fae4c87321568425ed5f8799520e7e62fb8665d1
snovacain <61154743+snovacain@users.noreply.github.com>
2023-08-18 18:31:33 -0700

    update

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ convert all when Convert is clicked..
 */
 
 
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+
+function convert() {
+    console.log("in the works")
+}
